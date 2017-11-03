b90a3c22235bf6154dcccd0a6676a6aeb578683b
bartoszgamza <bartosz.gamza12@gmail.com>
2017-11-03 14:30:07 +0100

    first commit

---
+++ app/resources/js/main.js
@@ -0,0 +1,10 @@
+
+//User click add button
+//If theres any text add that to the list
+document.getElementById('add').addEventListener('click', function(){
+  var value = document.getElementById('item').value
+  if (value){
+    
+  }
+
+});
