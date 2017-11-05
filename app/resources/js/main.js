00b520a8b4926da7bd9a8fe42bdfdd2af912fbdb
bartoszgamza <bartosz.gamza12@gmail.com>
2017-11-05 12:15:04 +0100

    48:51

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -3,8 +3,21 @@
 //If theres any text add that to the list
 document.getElementById('add').addEventListener('click', function(){
   var value = document.getElementById('item').value
-  if (value){
-    
-  }
+  if (value) addItemTodo(value);
+
 
 });
+
+function addItemTodo(item) {
+   var item = document.createElement('li');
+   item.innerText = text;
+
+   var buttons = document.createElement('div');
+   buttons.classList.add('buttons');
+
+   var remove = document.createElement('button');
+   remove.classList.add('remove');
+
+   var complete = document.createElement('button');
+   complete.classList.add('complete'); 
+}
